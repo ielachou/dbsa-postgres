1dd8eede8423adc04fc196a3918621bb5c4fc157
Iliass <iliass.el.achouchi@ulb.be>
2021-12-07 19:33:13 +0100

    xyz

--- src/backend/utils/adt/geo_selfuncs.c
+++ src/backend/utils/adt/geo_selfuncs.c
@@ -122,7 +122,6 @@ areajoinsel(PG_FUNCTION_ARGS)
     VariableStatData vardata2;
     Oid         opfuncoid;
     AttStatsSlot sslot1, sslot2;
-    int         nhist;
     RangeBound *hist_lower1, *hist_lower2;
     RangeBound *hist_upper1, *hist_upper2;
 	Datum *hist_freq1, *hist_freq2;
@@ -132,6 +131,7 @@ areajoinsel(PG_FUNCTION_ARGS)
     bool        join_is_reversed;
     bool        empty;
 
+    int nhist1, nhist2;
     int count1, count2;
 
 
@@ -162,10 +162,10 @@ areajoinsel(PG_FUNCTION_ARGS)
             PG_RETURN_FLOAT8((float8) selec);
         }
     }
-    nhist = sslot1.nvalues;
-    hist_lower1 = (RangeBound *) palloc(sizeof(RangeBound) * nhist);
-	hist_upper1 = (RangeBound *) palloc(sizeof(RangeBound) * nhist);
-	    for (i = 0; i < nhist; i++)
+    nhist1 = sslot1.nvalues;
+    hist_lower1 = (RangeBound *) palloc(sizeof(RangeBound) * nhist1);
+	hist_upper1 = (RangeBound *) palloc(sizeof(RangeBound) * nhist1);
+	    for (i = 0; i < nhist2; i++)
     {
         range_deserialize(typcache, DatumGetRangeTypeP(sslot1.values[i]),
                           &hist_lower1[i], &hist_upper1[i], &empty);
@@ -194,9 +194,10 @@ areajoinsel(PG_FUNCTION_ARGS)
             PG_RETURN_FLOAT8((float8) selec);
         }
     }
-    hist_upper2 = (RangeBound *) palloc(sizeof(RangeBound) * nhist);
-	hist_lower2 = (RangeBound *) palloc(sizeof(RangeBound) * nhist);
-    for (i = 0; i < nhist; i++)
+    nhist2 = sslot2.nvalues;
+    hist_upper2 = (RangeBound *) palloc(sizeof(RangeBound) * nhist2);
+	hist_lower2 = (RangeBound *) palloc(sizeof(RangeBound) * nhist2);
+    for (i = 0; i < nhist2; i++)
     {
         range_deserialize(typcache, DatumGetRangeTypeP(sslot2.values[i]),
                           &hist_lower2[i], &hist_upper2[i], &empty);
@@ -251,23 +252,23 @@ areajoinsel(PG_FUNCTION_ARGS)
 
     int currentLower1, currentLower2, currentUpper1, currentUpper2;
     int depth1, depth2;
-    depth1 = (DatumGetInt16(hist_upper1[101 -1].val) -1 
+    depth1 = (DatumGetInt16(hist_upper1[nhist1 -1].val) -1 
                             - DatumGetInt16(hist_lower1[0].val)) / 10;
-    depth2 = (DatumGetInt16(hist_upper2[101-1].val) -1 
+    depth2 = (DatumGetInt16(hist_upper2[nhist2-1].val) -1 
                             - DatumGetInt16(hist_lower2[0].val)) / 10;
     float mult = 0.0;
     for(i = 0; i < 10; i++){
         currentLower2 = (depth2 * i) + DatumGetInt16(hist_lower2[0].val);
         currentUpper2 = (depth2 * i) + DatumGetInt16(hist_lower2[0].val) + depth2;
         if(i == 9){
-            currentUpper2 = DatumGetInt16(hist_upper2[101-1].val);
+            currentUpper2 = DatumGetInt16(hist_upper2[nhist2-1].val);
         }
         for(int j = 0; j < 10; j++){
             currentLower1 = (depth1 * j) + DatumGetInt16(hist_lower1[0].val);
             currentUpper1 = (depth1 * j) + DatumGetInt16(hist_lower1[0].val) + depth1;
             
             if(j == 9){
-                currentUpper1 = DatumGetInt16(hist_upper1[101 -1].val);
+                currentUpper1 = DatumGetInt16(hist_upper1[nhist1 -1].val);
             }
             if(currentLower1 <= currentLower2){
                 if(currentUpper1 >= currentLower2){
